b03129470fc1c5f8877aa15ceb1caab6b4fd2cf6
john beltrez <beltrezjohn@gmail.com>
2020-08-04 09:52:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,25 @@
 
+class CashRegister 
+  attr_accessor :total, :discount
+  def initialize(discount=20)
+    @total= 0 
+    @discount= discount 
+    @items= []
+  end
+  def add_item(title, price, quantity= 1)
+    @total = @total + price * quantity
+    
+  end 
+  
+  def apply_discount 
+    @total = @total - @total * @discount / 100
+    if discount == 0 
+      "There is no discount to apply."
+      
+    else 
+     
+      "After the discount, the total comes to $#{@total}."
+    end 
+  end 
+  
+end 
