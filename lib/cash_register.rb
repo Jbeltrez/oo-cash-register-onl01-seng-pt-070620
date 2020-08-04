7eb3820807bf41bdcf0919d4810b868c75eed843
john beltrez <beltrezjohn@gmail.com>
2020-08-04 09:53:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,6 +8,9 @@ class CashRegister
   end
   def add_item(title, price, quantity= 1)
     @total = @total + price * quantity
+    quantity.times do 
+      @items << title 
+    end
     
   end 
   
